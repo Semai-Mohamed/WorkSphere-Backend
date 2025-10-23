e39ae6475c9ac382a36512b6bb2108591c66aa7f
Semai-Mohamed <semaimohamed860@gmail.com>
2025-10-23 18:42:24 +0200

    feat(auth): add @Cookies decorator to read token from HttpOnly cookie

---
+++ src/auth/strategies/token.strategy/cookies.decorator.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { createParamDecorator, ExecutionContext } from "node_modules/@nestjs/common";
+import { Request } from "node_modules/@types/express";
+
+
+export const Cookies = createParamDecorator(
+  (cookieName: string, ctx: ExecutionContext) => {
+    const request : Request= ctx.switchToHttp().getRequest();
+    // Return single cookie or all cookies
+    return cookieName ? request.cookies[cookieName] : request.cookies;
+  },
+);
